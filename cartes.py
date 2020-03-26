7b29278c6c0e2d834e9e0cd16b6b3cb0968974d5
tomstotom <62525382+tomstotom@users.noreply.github.com>
2020-03-26 18:50:20 +0100

    Add files via upload

--- cartes.py
+++ cartes.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 25 16:52:17 2020
+Created on Thu Mar 26 18:08:56 2020
 
+@author: Tom Stockholm
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 16:52:17 2020
 @author: Tom Stockholm
 """
 import random
@@ -53,6 +59,39 @@ class Joueur():
             cartes.montrer()
             
 
+
+#avec cette modélisation on peut faire nimporte quel jeu de carte, exemple d'un petit jeu de bataille.
+            
+
+
+jeu_en_cours = True
+
+while jeu_en_cours:
+    nom1 = input("Entrez le nom du joueur 1 :")
+    nom2 = input("Entrez le nom du joueur 2 :")
     
+    joueur1 = Joueur(nom1)
+    joueur2 = Joueur(nom2)
+    
+    deck = Deck()
+    deck.contruire()
+    deck.melanger()
 
+    joueur1.tirer_carte(deck)
+    joueur2.tirer_carte(deck)
 
+    print(f"{joueur1.prenom} a pioché un : ")
+    joueur1.montrer_main()
+    print(f"{joueur2.prenom} a pioché un : ")
+    joueur2.montrer_main()
+
+    cj1 = joueur1.main[0]
+    cj2 = joueur2.main[0]
+    
+    if  cj1.valeur > cj2.valeur:
+        print(f"{joueur1.prenom} gagne")
+    else:
+        print(f"{joueur2.prenom} gagne")
+    jeu_en_cours = False
+
+            
