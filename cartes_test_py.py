daf0cef7f4092e56f1abf861b931e12ef1909712
tomstotom <62525382+tomstotom@users.noreply.github.com>
2020-03-26 18:56:02 +0100

    Add files via upload

---
+++ cartes_test_py.py
@@ -0,0 +1,11 @@
+
+            
+if __name__ == "__main__":
+    import doctest
+    if False:
+        doctest.testmod(verbose=True, optionflags=512)
+    else:
+        doctest.testmod(verbose=True)
+            
+        
+        
